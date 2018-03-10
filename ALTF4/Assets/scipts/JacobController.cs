74cc8bb21c567dce88278332526ec92c5e8bf787
Douvry <valentin.jabba@gmail.com>
2018-03-10 02:17:46 -0500

    Ajou perso / Anim Marche - saut / level 1

--- ALTF4/Assets/scipts/JacobController.cs
+++ ALTF4/Assets/scipts/JacobController.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class JacobController : MonoBehaviour {
 
     Rigidbody corps;
-    public int speed = 10;
+    public float speed = 10;
     bool seDeplace;
+    public float forceSaut = 100;
+    Animator animateur;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    private bool grounded = false;
+    private float rayonGround = 0.2f;
+    private float move;
+    
+
+
+
     private void Awake()
     {
-        corps = GetComponent<Rigidbody>();
+        corps = this.GetComponent<Rigidbody>();
+        animateur = this.GetComponent<Animator>();
     }
     // Use this for initialization
     void Start()
@@ -21,12 +33,24 @@ public class JacobController : MonoBehaviour {
     void Update()
     {
 
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animateur.SetBool("grounded", false);
+            //corps.AddForce(new Vector3(0, forceSaut));
+            
+        }
+
+
 
     }
 
     public void FixedUpdate()
     {
+        var vel = corps.velocity;
+        speed = vel.magnitude;
+        animateur.SetFloat("speed", speed);
+        grounded = Physics.CheckSphere(groundCheck.position, rayonGround, whatIsGround);
+        animateur.SetBool("grounded", grounded);
         seDeplace = false;
         Vector3 direction = new Vector3();
 
@@ -34,12 +58,16 @@ public class JacobController : MonoBehaviour {
         {
             direction.z += 1f;
             seDeplace = true;
+            
+            
+            
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             direction.z -= 1f;
             seDeplace = true;
+            
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -52,11 +80,15 @@ public class JacobController : MonoBehaviour {
         {
             direction.x += 1f;
             seDeplace = true;
+            
         }
+        
 
         if (seDeplace)
         {
+            speed = 100;
             corps.velocity = (direction * Time.deltaTime * speed);
+            
         }
         /*else
         {
