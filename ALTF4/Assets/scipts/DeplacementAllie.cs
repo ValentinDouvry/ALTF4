34c5094faacdf1dd667d6725292da69803a829a3
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-03-09 21:37:40 -0500

    Deplacement Allié

---
+++ ALTF4/Assets/scipts/DeplacementAllie.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementAllie : MonoBehaviour
+{
+
+    Rigidbody corps;
+    public int speed = 10;
+    bool seDeplace;
+    private void Awake()
+    {
+        corps = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        seDeplace = false;
+        Vector3 direction = new Vector3();
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction.z += 0.1f;
+            seDeplace = true;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction.z -= 0.1f;
+            seDeplace = true;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction.x -= 0.1f;
+            seDeplace = true;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction.x += 0.1f;
+            seDeplace = true;
+        }
+
+        if (seDeplace)
+        {
+            corps.AddForce(direction * Time.deltaTime * speed);
+        }
+        else
+        {
+            corps.velocity = Vector3.zero;
+            corps.angularVelocity = Vector3.zero;
+        }
+
+    }
+}
