e0f523fd79b8981353b60d1e796d468e9e0c6c8e
Douvry <valentin.jabba@gmail.com>
2018-03-10 18:57:05 -0500

    Ajout spike test

---
+++ ALTF4/Assets/scripts/CollisionSpikes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSpikes : MonoBehaviour {
+
+
+    GameObject spikes;
+    Animator animator;
+    // Use this for initialization
+    void Awake () {
+        spikes = GameObject.FindGameObjectWithTag("anim_spikeSpike");
+        animator = spikes.GetComponent<Animator>();
+        animator.SetBool("spikeSortir", false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Jacob")
+        {
+            
+            animator.SetBool("spikeSortir", true);
+
+        }
+        animator.SetBool("spikeSortir", false);
+    }
+}
