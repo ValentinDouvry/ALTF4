752ada87f19b087d16a2b0fec8c5e79add1a24e6
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-03-10 10:15:03 -0500

    Changement dossier

--- ALTF4/Assets/scripts/DeplacementAllie.cs
+++ ALTF4/Assets/scripts/DeplacementAllie.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class DeplacementAllie : MonoBehaviour
 {
 
-    Rigidbody corps;
+    Rigidbody corps, corps2;
     public Vector3 speed;
     //public int speed = 10;
     float decelTime = 2;
     float currentDecelTime = 0;
-    bool seDeplace;
+    //bool seDeplace;
     public bool estMort = false;
     Camera cam1, cam2;
+    GameObject piege1;
+    GameObject[] tuiles = new GameObject[29];
+    EnigmeTuiles
     
 
     private void Awake()
@@ -26,11 +29,15 @@ public class DeplacementAllie : MonoBehaviour
         cam2 = GameObject.Find("CameraMort").GetComponent<Camera>();
         cam1.enabled = true;
         cam2.enabled = false;
+        piege1 = GameObject.Find("Piege1");
+        corps2 = piege1.GetComponent<Rigidbody>();
+        tuiles = GameObject.FindGameObjectsWithTag("Tuiles");
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (!seDeplace)
         {
             float interpolatinFactor = currentDecelTime / decelTime;
@@ -38,49 +45,52 @@ public class DeplacementAllie : MonoBehaviour
             transform.position -= move;
             currentDecelTime += Time.deltaTime;
         }
-
-
+        */
     }
 
     public void FixedUpdate()
     {
-        seDeplace = false;
+        //seDeplace = false;
         Vector3 direction = new Vector3();
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             direction.z += 1f;
-            seDeplace = true;
+            //seDeplace = true;
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             direction.z -= 1f;
-            seDeplace = true;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            direction.x -= 1f; seDeplace = true;
+            direction.x -= 1f;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            direction.x += 1f; seDeplace = true;
+            direction.x += 1f;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            direction.y += 1f; seDeplace = true;
+            direction.y += 1f;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            direction.y -= 1f; seDeplace = true;
+            direction.y -= 1f;
+            //seDeplace = true;
 
         }
 
@@ -98,9 +108,38 @@ public class DeplacementAllie : MonoBehaviour
         }
     }
 
+    private IEnumerator OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Action" && Input.GetKey(KeyCode.E))
+        {
+            corps2.AddForce(0, 0, 200);
+            yield return new WaitForSeconds(2);
+            corps2.velocity = Vector3.zero;
+            corps2.angularVelocity = Vector3.zero;
+        }
+        if(collision.gameObject.tag == "Tuile")
+        {
+            GameObject tuileCourante = collision.gameObject;
+            var a = tuileCourante.GetComponent<EnigmeTuiles>();
+            if (a.getEstValable())
+            {
+                a.setEstValable(false);
+                setTuileSuivante(a.name);
+            }
+            {
+
+            }
+        }
+    }
+
     private void OnCollisionExit(Collision collision)
     {
         cam1.enabled = true;
         cam2.enabled = false;
+    }
+
+    private void setTuileSuivante(string tuileCourante)
+    {
+        int.Parse(tuileCourante);
     }
 }
---
+++ ALTF4/Assets/scripts/EnigmeTuiles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnigmeTuiles : MonoBehaviour {
+
+    public bool estValable;
+    Rigidbody corps;
+
+    private void Awake()
+    {
+        corps = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (estValable)
+        {
+            //renderer.material.color = red;
+        }
+	}
+
+    public void setEstValable(bool valeur)
+    {
+        estValable = valeur;
+    }
+
+    public bool getEstValable()
+    {
+        return estValable;
+    }
+}
