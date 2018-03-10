7d3269ded0b8a139ec2eeaa3b0533e530f2a4e5d
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-03-10 09:05:47 -0500

    Bouton d'activation de piège (Mur qui bouge)

--- ALTF4/Assets/scipts/DeplacementAllie.cs
+++ ALTF4/Assets/scipts/DeplacementAllie.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class DeplacementAllie : MonoBehaviour
 {
 
-    Rigidbody corps;
+    Rigidbody corps, corps2;
     public Vector3 speed;
     //public int speed = 10;
     float decelTime = 2;
     float currentDecelTime = 0;
-    bool seDeplace;
+    //bool seDeplace;
     public bool estMort = false;
     Camera cam1, cam2;
+    GameObject piege1;
     
 
     private void Awake()
@@ -26,11 +27,14 @@ public class DeplacementAllie : MonoBehaviour
         cam2 = GameObject.Find("CameraMort").GetComponent<Camera>();
         cam1.enabled = true;
         cam2.enabled = false;
+        piege1 = GameObject.Find("Piege1");
+        corps2 = piege1.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (!seDeplace)
         {
             float interpolatinFactor = currentDecelTime / decelTime;
@@ -38,49 +42,52 @@ public class DeplacementAllie : MonoBehaviour
             transform.position -= move;
             currentDecelTime += Time.deltaTime;
         }
-
-
+        */
     }
 
     public void FixedUpdate()
     {
-        seDeplace = false;
+        //seDeplace = false;
         Vector3 direction = new Vector3();
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             direction.z += 1f;
-            seDeplace = true;
+            //seDeplace = true;
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             direction.z -= 1f;
-            seDeplace = true;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            direction.x -= 1f; seDeplace = true;
+            direction.x -= 1f;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            direction.x += 1f; seDeplace = true;
+            direction.x += 1f;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            direction.y += 1f; seDeplace = true;
+            direction.y += 1f;
+            //seDeplace = true;
 
         }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            direction.y -= 1f; seDeplace = true;
+            direction.y -= 1f;
+            //seDeplace = true;
 
         }
 
@@ -95,6 +102,17 @@ public class DeplacementAllie : MonoBehaviour
             cam2.enabled = true;
             yield return new WaitForSeconds(2);
             transform.position = new Vector3(0,0,0);
+        }
+    }
+
+    private IEnumerator OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Action" && Input.GetKey(KeyCode.E))
+        {
+            corps2.AddForce(0, 0, 200);
+            yield return new WaitForSeconds(2);
+            corps2.velocity = Vector3.zero;
+            corps2.angularVelocity = Vector3.zero;
         }
     }
 
